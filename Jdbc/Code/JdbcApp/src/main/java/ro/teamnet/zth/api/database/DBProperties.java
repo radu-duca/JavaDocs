4e3381673dd291b74ef4e14ef78d9d838ba7e105
ralucar <raluca.russindilar@teamnet.ro>
2016-07-13 13:13:36 +0300

    Added app for reflection + jdbc workshops

---
+++ Jdbc/Code/JdbcApp/src/main/java/ro/teamnet/zth/api/database/DBProperties.java
@@ -0,0 +1,15 @@
+package ro.teamnet.zth.api.database;
+
+/**
+ * Created by Raluca.Russindilar on 02.07.2016.
+ */
+public interface DBProperties {
+    String IP = "192.168.99.100";
+    String PORT = "49161";
+    //TODO de inlocuit cu utilizatorul vostru
+    String USER = "ralucar";
+    //TODO de inlocuit cu parola voastra
+    String PASS = "ralucar";
+    String DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
+
+}
